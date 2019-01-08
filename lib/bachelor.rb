2a875bd9f634f6f24c3f86680c40dc01e0a87e7c
Daniel Toma <danieltoma@ubalumni.com>
2019-01-08 01:05:12 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,63 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+
+  season_contestant_bios = data.select {|year, values| year.to_s == season}
+  season_contestant_bios.each do |season, profile| 
+    profile.each do |hash|
+      if hash["status"] == "Winner"
+        return hash["name"].split(/ /).first
+      end
+    end
+  end
+
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  data.each do |season, contestant_bios|
+    contestant_bios.each do |hash|
+      if hash["occupation"] == occupation
+        return hash["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  counter = 0
+  data.each do |season, contestant_bios|
+    contestant_bios.each do |hash|
+      if hash["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  return counter
 end
 
 def get_occupation(data, hometown)
   # code here
+  data.each do |season, contestant_bios|
+    contestant_bios.each do |hash|
+      if hash["hometown"] == hometown
+        return hash["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
   # code here
 end
+  counter = 0
+  total = 0.0
+  season_contestant_bios = data.select {|year, values| year.to_s == season}
+  season_contestant_bios.each do |season, profile| 
+    profile.each do |hash|
+      counter += 1 
+      total += hash["age"].to_f
+    end
+  end
+  return (total / counter).round
+end 
